78f01f5cdfb228f6b3cb7ca746d9fec74d9b2652
Jose Castro <31970492+drakonc@users.noreply.github.com>
2021-09-27 19:17:45 -0500

    Agregando Clase Ruta

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoriesController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\SettingsController;
