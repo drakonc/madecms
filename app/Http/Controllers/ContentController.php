b275be7d89baad030a2a3518bc3f0e33a937699a
Jose Castro <drakonc@gmail.com>
2022-09-30 08:10:07 -0500

    cookie

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\{Request, Response};
 use App\Models\Category;
 use App\Models\Slider;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Str;
 
 class ContentController extends Controller
 {
@@ -14,7 +15,7 @@ class ContentController extends Controller
         $categories = Category::where('module','0')->orderBy('name','Asc')->get();
         $sliders = Slider::where('status', 1)->orderby('sorder','Asc')->get();
         $data = ['categories'=> $categories, 'sliders'=>$sliders];
-        Cookie::queue(Cookie::make('name',auth()->user()->id,$minutes));
+        Cookie::queue(Cookie::make('name',Str::random(8),$minutes));
         return view('home',$data);
     }
 
